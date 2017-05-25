f2ccf1ea48374ca0d0044b082cc83adde29d6a61
akell47 <amberkeller47@gmail.com>
2017-05-24 23:22:37 -0400

    dna start

---
+++ dna.py
@@ -0,0 +1,10 @@
+# DNA DioxyriboNucleic Acid
+# A, C, G, T - four nucleotides to construct DNA
+# A - adenine
+# C - cytosine
+# G - guanine
+# T - thymine
+# DNA -> RNA -> Protein
+# one amino acid = nucleotide triplet
+# sequence of amino acid = protein - comprised of 20 amino acids
+# dictionary of life
