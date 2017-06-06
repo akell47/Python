0cfc40028f2b6a39275e523154fa61ac0459f145
Amber Keller <amber.keller@dunwoodyga.gov>
2017-06-06 11:17:04 -0400

    move files

---
+++ Language/language.py
@@ -0,0 +1,10 @@
+"""
+Examine Properties of individual books
+    - book lenth
+    - number of unique words
+    - how attributes cluster by language or authorship
+
+Project Gutenberg - oldest digital library of books with over 50,000 books
+are in a public domain - can be downloaded and read for free
+
+"""
